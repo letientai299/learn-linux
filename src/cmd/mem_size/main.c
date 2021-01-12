6c6820e1f3a5b009fcbd9aefddaf764c1d69a846
Tai <letientai299@gmail.com>
2021-01-12 14:19:07 +0800

    Init project

---
+++ src/cmd/mem_size/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("this is mem_size");
+    return 0;
+}
---
+++ src/cmd/page_size/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+  printf("this is page_size");
+  return 0;
+}
